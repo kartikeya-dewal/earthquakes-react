80d1c22d0b729643a920a6d92a0aebe2edab256d
kartikeya-dewal <kartikeya.dewal@gmail.com>
2019-06-20 15:01:09 +1000

    allow cross origin

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const createError = require('http-errors');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./data/schema');
 
@@ -11,7 +12,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(cors());
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   graphiql: true
